8c2d6369746e44a4a5ddbc30ab7603a4616816c6
Lakshmi Narayana <gnarayana122@gmail.com>
2026-07-09 13:18:53 +0530

    hero image updated

--- src/components/hero/HeroHeader.jsx
+++ src/components/hero/HeroHeader.jsx
@@ -1,65 +1,93 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
 
-import hero from "../../assets/hero.svg";
+import hero from "../../assets/hero.png";
 import path from "../../assets/Path.svg";
 import downArrow from "../../assets/down-arrow.png";
 
 function HeroHeader() {
   return (
     <section className="w-full bg-white overflow-hidden">
-      <div className="relative max-w-[1440px] mx-auto min-h-screen">
-        
+      <div className="relative max-w-[1440px] mx-auto">
+
+        {/* Background Shape */}
         <img
           src={path}
           alt=""
-          className="hidden lg:block absolute top-0 right-0 w-[45%] max-w-[650px] pointer-events-none z-0"
+          className="
+            hidden
+            lg:block
+            absolute
+            top-0
+            right-0
+            w-[38%]
+            xl:w-[42%]
+            2xl:w-[45%]
+            pointer-events-none
+            select-none
+            z-0
+          "
         />
 
         <Navbar />
 
-        
         <div
           className="
-            relative z-10
-            flex flex-col
-            lg:flex-row
+            relative
+            z-10
+
+            grid
+            lg:grid-cols-2
+
             items-center
-            justify-center
-            lg:justify-between
-            gap-12 lg:gap-0
+
+            gap-16
+            lg:gap-8
+
             px-5
             sm:px-8
             md:px-12
-            lg:px-[72px]
+            lg:px-16
+            xl:px-[72px]
+
             pt-10
-            md:pt-16
-            lg:pt-24
+            sm:pt-16
+            lg:pt-20
+
             pb-16
+            lg:pb-24
           "
         >
-          
+
+          {/* Left Content */}
+
           <div
             className="
-              w-full
-              md:w-[80%]
-              lg:w-[42%]
+              order-2
+              lg:order-1
+
               text-center
               lg:text-left
+
+              max-w-[620px]
               mx-auto
+              lg:mx-0
             "
           >
+
             <h1
               className="
                 font-bold
                 text-[#232340]
-                tracking-[-1px]
-                leading-tight
-                text-5xl
-                sm:text-6xl
-                md:text-7xl
-                lg:text-[84px]
-                lg:leading-[84px]
+                tracking-tight
+                leading-[1.05]
+
+                text-4xl
+                sm:text-5xl
+                md:text-6xl
+                lg:text-7xl
+                xl:text-[84px]
+                xl:leading-[84px]
               "
             >
               Stand Out
@@ -72,11 +100,16 @@ function HeroHeader() {
             <p
               className="
                 mt-6
+
                 text-[#606060]
+
                 text-base
                 sm:text-lg
+
                 leading-7
-                max-w-lg
+
+                max-w-[520px]
+
                 mx-auto
                 lg:mx-0
               "
@@ -88,22 +121,41 @@ function HeroHeader() {
             <button
               className="
                 mt-8
-                bg-[#5468E7]
-                hover:bg-[#4457d6]
-                text-white
-                font-bold
-                rounded-xl
+
+                h-14
+
                 w-full
                 sm:w-[220px]
-                h-14
-                transition
+
+                rounded-xl
+
+                bg-[#5468E7]
+
+                hover:bg-[#4457d6]
+
+                text-white
+
+                font-bold
+
+                transition-all
+                duration-300
               "
             >
               Recent Work
             </button>
 
-            
-            <div className="hidden lg:flex items-center gap-4 mt-20">
+            <div
+              className="
+                hidden
+                lg:flex
+
+                items-center
+
+                gap-4
+
+                mt-20
+              "
+            >
               <img
                 src={downArrow}
                 alt="Scroll Down"
@@ -114,59 +166,84 @@ function HeroHeader() {
                 Scroll down
               </p>
             </div>
+
           </div>
 
-          
-            <div
-                className="
-                    relative
-                    w-full
-                    md:w-[80%]
-                    lg:w-[58%]
-                    flex
-                    justify-center
-                    items-center
-                    mx-auto
-                    mt-10
-                    lg:mt-0
-                "
-            >
-            
-                <div
-                    className="
-                    absolute
-                    inset-0
-                    m-auto
-                    w-[70%]
-                    h-[85%]
-                    bg-[#C897E4]
-                    rounded-[30px]
-                    lg:w-[570px]
-                    lg:h-[722px]
-                    lg:rounded-[40px]
-                    lg:right-6
-                    lg:top-0
-                    lg:left-auto
-                    lg:m-0
-                    "
-                ></div>
+          {/* Right Image */}
 
-            
-                <img
-                    src={hero}
-                    alt="Hero Illustration"
-                    className="
-                    relative
-                    z-10
-                    w-full
-                    max-w-[750px]
-                    lg:max-w-[924px]
-                    h-auto
-                    mx-auto
-                    "
-                />
-            </div>
+          <div
+            className="
+              order-1
+              lg:order-2
+
+              relative
+
+              flex
+
+              justify-center
+              items-center
+            "
+          >
+
+            {/* Purple Card */}
+
+            <div
+              className="
+                absolute
+
+                w-[75%]
+                h-[82%]
+
+                rounded-[30px]
+
+                bg-[#C897E4]
+
+                sm:w-[70%]
+
+                lg:w-[70%]
+                lg:h-[82%]
+
+                xl:w-[560px]
+                xl:h-[710px]
+
+                xl:rounded-[40px]
+
+                right-auto
+                lg:right-4
+              "
+            />
+
+            {/* Hero */}
+
+            <img
+              src={hero}
+              alt="Hero Illustration"
+              className="
+                relative
+
+                z-10
+
+                w-full
+
+                max-w-[320px]
+                sm:max-w-[450px]
+                md:max-w-[560px]
+                lg:max-w-[650px]
+                xl:max-w-[850px]
+
+                h-auto
+
+                object-contain
+
+                select-none
+                pointer-events-none
+              "
+            />
+
+          </div>
+
         </div>
+
       </div>
     </section>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import logo from "../../assets/light-mode.svg";
-import { Link as ScrollLink } from "react-scroll";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,23 +14,27 @@ function Navbar() {
     }`;
 
   return (
-    <nav className="relative z-50 w-full bg-white">
+    <nav className="sticky top-0 z-50 ">
       <div className="max-w-[1296px] mx-auto px-4 sm:px-6 lg:px-8">
 
-        
-        <div className="h-16 md:h-20 flex items-center justify-between">
+        {/* Navbar */}
+        <div className="flex items-center justify-between h-16 lg:h-20">
 
-          
-          <NavLink to="/">
+          {/* Logo */}
+          <NavLink
+            to="/"
+            className="flex items-center shrink-0"
+          >
             <img
               src={logo}
               alt="Folio Logo"
-              className="h-8 md:h-10"
+              className="h-8 sm:h-9 lg:h-10 w-auto"
             />
           </NavLink>
 
-          
-          <ul className="hidden md:flex items-center gap-6 lg:gap-12">
+          {/* Desktop Menu */}
+          <ul className="hidden md:flex items-center gap-8 lg:gap-12">
+
             <li>
               <NavLink to="/" end className={linkClass}>
                 About
@@ -54,6 +57,7 @@ function Navbar() {
             </li>
 
             <li className="relative">
+
               <Link
                 to="/#jobs"
                 className="font-semibold text-[#606060] hover:text-[#5468E7] transition-colors duration-300"
@@ -61,41 +65,105 @@ function Navbar() {
                 Jobs
               </Link>
 
-              <span className="absolute -top-2 -right-4 w-5 h-5 rounded-full bg-[#5468E7] text-white text-xs flex items-center justify-center">
+              <span
+                className="
+                  absolute
+                  -top-2
+                  -right-4
+                  flex
+                  items-center
+                  justify-center
+                  w-5
+                  h-5
+                  rounded-full
+                  bg-[#5468E7]
+                  text-white
+                  text-[10px]
+                  font-semibold
+                "
+              >
                 2
               </span>
+
             </li>
+
           </ul>
 
-          
-          <button className="hidden md:block bg-[#232340] text-white font-bold rounded-xl px-6 lg:px-8 py-3 hover:bg-[#5468E7] transition">
+          {/* Desktop Button */}
+
+          <button
+            className="
+              hidden
+              md:flex
+              items-center
+              justify-center
+              px-6
+              lg:px-8
+              py-3
+              rounded-xl
+              bg-[#232340]
+              text-white
+              font-bold
+              transition
+              hover:bg-[#5468E7]
+            "
+          >
             Contact Us
           </button>
 
-          
+          {/* Mobile Toggle */}
+
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="md:hidden"
+            aria-label="Toggle navigation"
+            aria-expanded={isOpen}
+            className="
+              md:hidden
+              flex
+              items-center
+              justify-center
+              w-11
+              h-11
+              rounded-lg
+              hover:bg-gray-100
+              transition
+            "
           >
-            {isOpen ? (
-              <X size={28} />
-            ) : (
-              <Menu size={28} />
-            )}
+            {isOpen ? <X size={28} /> : <Menu size={28} />}
           </button>
+
         </div>
 
-        
-        {isOpen && (
-          <div className="md:hidden bg-white shadow-lg rounded-xl mt-2 p-6">
-            <ul className="flex flex-col gap-6">
+        {/* Mobile Menu */}
+
+        <div
+          className={`
+            md:hidden
+            overflow-hidden
+            transition-all
+            duration-300
+            ${isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"}
+          `}
+        >
+
+          <div
+            className="
+              mt-2
+              rounded-2xl
+              p-5
+            "
+          >
+
+            <ul className="flex flex-col">
 
               <li>
                 <NavLink
                   to="/"
                   end
-                  className={linkClass}
                   onClick={() => setIsOpen(false)}
+                  className={({ isActive }) =>
+                    `${linkClass({ isActive })} block py-3`
+                  }
                 >
                   About
                 </NavLink>
@@ -104,8 +172,10 @@ function Navbar() {
               <li>
                 <NavLink
                   to="/work"
-                  className={linkClass}
                   onClick={() => setIsOpen(false)}
+                  className={({ isActive }) =>
+                    `${linkClass({ isActive })} block py-3`
+                  }
                 >
                   Work
                 </NavLink>
@@ -114,31 +184,73 @@ function Navbar() {
               <li>
                 <Link
                   to="/#services"
-                  className="font-semibold text-[#606060] hover:text-[#5468E7] transition-colors duration-300"
+                  onClick={() => setIsOpen(false)}
+                  className="
+                    block
+                    py-3
+                    font-semibold
+                    text-[#606060]
+                    hover:text-[#5468E7]
+                  "
                 >
                   Services
                 </Link>
               </li>
 
-              <li className="relative">
+              <li className="flex items-center justify-between py-3">
+
                 <Link
                   to="/#jobs"
-                  className="font-semibold text-[#606060] hover:text-[#5468E7] transition-colors duration-300"
+                  onClick={() => setIsOpen(false)}
+                  className="
+                    font-semibold
+                    text-[#606060]
+                    hover:text-[#5468E7]
+                  "
                 >
                   Jobs
                 </Link>
 
-                <span className="absolute -top-2 -right-4 w-5 h-5 rounded-full bg-[#5468E7] text-white text-xs flex items-center justify-center">
+                <span
+                  className="
+                    flex
+                    items-center
+                    justify-center
+                    w-5
+                    h-5
+                    rounded-full
+                    bg-[#5468E7]
+                    text-white
+                    text-[10px]
+                  "
+                >
                   2
                 </span>
+
               </li>
 
-              <button className="w-full bg-[#232340] text-white font-bold rounded-xl py-3">
-                Contact Us
-              </button>
             </ul>
+
+            <button
+              className="
+                mt-5
+                w-full
+                py-3
+                rounded-xl
+                bg-[#232340]
+                text-white
+                font-bold
+                transition
+                hover:bg-[#5468E7]
+              "
+            >
+              Contact Us
+            </button>
+
           </div>
-        )}
+
+        </div>
+
       </div>
     </nav>
   );
