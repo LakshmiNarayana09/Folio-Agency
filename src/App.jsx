2e6c3c4ba456901a8eae8764935d68583842a9b7
Lakshmi Narayana <gnarayana122@gmail.com>
2026-07-09 10:58:55 +0530

    navigation updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import CaseStudy from './pages/CaseStudy'
+import Jobs from "./components/jobs/Jobs";
+import Services from "./components/services/Services";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<CaseStudy />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/services" element={<Services />} />
         </Routes>
       </BrowserRouter>
     </div>
